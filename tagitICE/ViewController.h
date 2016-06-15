a1a8d7ca6b1d5297c2e6f40017d6429942e6af48
Suhas <suhas.patil@sandstechnologies.in>
2016-06-15 19:21:26 +0530

    Initial Commit

---
+++ tagitICE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tagitICE
+//
+//  Created by rac on 15/06/16.
+//  Copyright © 2016 Sands Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tagitICE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tagitICE
+//
+//  Created by rac on 15/06/16.
+//  Copyright © 2016 Sands Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
